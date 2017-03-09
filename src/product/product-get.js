813a5b72f7c41dc6c4762fc69eece257044800bc
abkreno <mohamedelhamamsy789@gmail.com>
2017-03-09 21:22:48 +0200

    Adds product get

--- src/product/product-get.js
+++ src/product/product-get.js
@@ -8,11 +8,13 @@
  * @apiSuccess {String} name Firstname of the User
  * @apiSuccess {String} description Description of the product
  * @apiSuccess {Number} createdAt Product time of creation (UNIX)
+ * @apiSuccess {Number} updatedAt Product time of last update (UNIX)
  * @apiSuccess {Object[]}  items Array of different types of the same product
  * @apiSuccess {Number} items.size Item's Size
  * @apiSuccess {String} items.color Item's color
  * @apiSuccess {Double} items.price Item's Price
  * @apiSuccess {Number} items.createdAt Item time of creation (UNIX)
+ * @apiSuccess {Number} vendorId Vendor ID
  * @apiErrorExample {json} Error-Response:
  *     HTTP/1.1 404 Not Found
  *     {
