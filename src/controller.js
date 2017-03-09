f945a27a5f1fc685afabbe3fa7ae7642a1a28eaf
Mohamed Mehany <mohamed.mehany@dfki.de>
2017-03-09 19:04:50 +0200

    Initital Commit

---
+++ src/controller.js
@@ -0,0 +1,16 @@
+/**
+ * @api {get} /product/:id Request Product information
+ * @apiName GetProduct
+ * @apiGroup Product
+ *
+ * @apiParam {Number} id Product's unique ID.
+ *
+ * @apiSuccess {String} name Firstname of the User.
+ * @apiSuccess {String} description Description of the product.
+ * @apiSuccess {Number} createdAt Product time of creation (UNIX)
+ * @apiSuccess {Object[]}  items Array of different types of the same product
+ * @apiSuccess {Number} items.size Item's Size
+ * @apiSuccess {String} items.color Item's color
+ * @apiSuccess {Double} items.price Item's Price
+ * @apiSuccess {Number} items.createdAt Item time of creation (UNIX)
+ */
