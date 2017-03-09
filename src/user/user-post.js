cbb86c165dbc8abc69c382329f755ca98d19bee8
abkreno <mohamedelhamamsy789@gmail.com>
2017-03-09 21:54:52 +0200

    Removes token from user get

--- src/user/user-post.js
+++ src/user/user-post.js
@@ -9,11 +9,26 @@
  * @apiParam {Number} dateOfBirth User date of birth (UNIX)
  * @apiParam {Number} gender Gender of the user
  *
- * @apiSuccess {String} token User authentication token
  * @apiSuccess {Number} id ID of the User
  * @apiErrorExample {json} Error-Response:
  *     HTTP/1.1 400 Bad Request
  *     {
  *       "error": "InvalidEmail"
+ *     }
+ */
+/**
+ * @api {Post} /login/ Login with an existing User
+ * @apiName Login
+ * @apiGroup User
+ *
+ * @apiParam {String} email Email of the User
+ * @apiParam {String} password Password of the User
+ *
+ * @apiSuccess {String} token User authentication token
+ * @apiSuccess {Number} id ID of the User
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "InCorrectPassword"
  *     }
  */
