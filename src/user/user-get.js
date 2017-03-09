9b1efda9cc79cb55f123faa916524b741c51a1ae
abkreno <mohamedelhamamsy789@gmail.com>
2017-03-09 22:34:33 +0200

    Modifies user post/get

--- src/user/user-get.js
+++ src/user/user-get.js
@@ -12,7 +12,6 @@
  * @apiSuccess {Number} gender Gender of the user
  * @apiSuccess {Number} createdAt User time of creation (UNIX)
  * @apiSuccess {Number} updatedAt User time of last update (UNIX)
- * @apiSuccess {String} token User authentication token
  * @apiSuccess {Number} id ID of the User
  * @apiSuccess {Object[]} items Array of items owned by the user
  * @apiSuccess {Number} items.id Item ID
@@ -24,4 +23,38 @@
  *       "error": "UserNotFound"
  *     }
  */
- 
+
+/**
+ * @api {get} /user/ Request Current User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiHeader {String} authorization Authorization token.
+ *
+ * @apiSuccess {String} name Firstname of the User
+ * @apiSuccess {String} email Email of the User
+ * @apiSuccess {String} address Address of the User
+ * @apiSuccess {Number} dateOfBirth User date of birth (UNIX)
+ * @apiSuccess {Number} gender Gender of the user
+ * @apiSuccess {Number} createdAt User time of creation (UNIX)
+ * @apiSuccess {Number} updatedAt User time of last update (UNIX)
+ * @apiSuccess {String} token User authentication token
+ * @apiSuccess {Number} id ID of the User
+ * @apiSuccess {Object[]} items Array of items owned by the user
+ * @apiSuccess {Number} items.id Item ID
+ * @apiSuccess {String} items.productId Item Product ID
+ * @apiSuccess {Number} items.createdAt Item time of buying (UNIX)
+ * @apiSuccess {Object[]} bankingInfo Array of the user's banking info
+ * @apiSuccess {String} bankingInfo.cardNumber Card Number
+ * @apiSuccess {String} bankingInfo.cardHolder Name of the User that owns the card
+ * @apiSuccess {String} bankingInfo.provider The Provider of the card
+ * @apiSuccess {Number} bankingInfo.type Type of the card
+ * @apiSuccess {Number} bankingInfo.createdAt BankingInfo time of creation (UNIX)
+ * @apiSuccess {Number} bankingInfo.updatedAt BankingInfo time of creation (UNIX)
+ * @apiSuccess {Number} bankingInfo.id ID of the Banking Info
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
--- src/user/user-post.js
+++ src/user/user-post.js
@@ -32,3 +32,24 @@
  *       "error": "InCorrectPassword"
  *     }
  */
+ /**
+  * @api {Post} /user/:userId/bankingInfo/ Create a new Banking for the current User
+  * @apiName CreateUserBankingInfo
+  * @apiGroup BankingInfo
+  *
+  * @apiHeader {String} authorization Authorization token.
+  *
+  * @apiParam {String} cardNumber Card Number
+  * @apiParam {String} cardHolder Name of the User that owns the card
+  * @apiParam {String} provider The Provider of the card
+  * @apiParam {Number} type Type of the card
+  * @apiParam {String} name Name of the Department
+  * @apiParam {String} description Description of the Department
+  *
+  * @apiSuccess {Number} id ID of the BankingInfo
+  * @apiErrorExample {json} Error-Response:
+  *     HTTP/1.1 400 Bad Request
+  *     {
+  *       "error": "InvalidDepartmentName"
+  *     }
+  */
